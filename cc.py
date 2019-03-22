74d262d2627076a8286d240dcf952e7b7f35d4bd
Alfie Foster <bonzi@lordbonzi.pro>
2019-03-22 21:32:47 +0000

    Look Ma, I did a thing

---
+++ cc.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+room = "Room 1" # Room Name
+frab = "https://timetable.flawcon.xyz/2019/schedule/export/schedule.json" # Timetable/Schedule URL
+
+sched = requests.get(frab).json()["schedule"]["conference"]["days"][0]["rooms"]
+
+z = ""
+
+for i in sched[room]:
+    print("From-until	" + i["title"])
+
+    if len(i["persons"]) == 1:
+        for y in i["persons"]:
+            z = z + y["name"]
+    elif len(i["persons"]) > 1:
+        for y in i["persons"][:-1]:
+            z = z + y["name"] + ", "
+        z = z + i["persons"][len(i["persons"])-1]["name"]
+    else:
+        print("Error")
+
+    print("From-until	- " + z)
+    z = ""
